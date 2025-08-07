1935f5a5471f686bca17dd6383d0d8c3c24fa164
Gnanendra P <gnanendrapnani@gmail.com>
2025-08-07 08:41:41 +0530

    add, edit, delete, fetch of Product api are Created

--- controllers/admin/productsController.js
+++ controllers/admin/productsController.js
@@ -1,5 +1,7 @@
 const { imageUploadUtil } = require("../../helpers/cloudinary");
+const Product = require("../../models/Product");
 
+// These controller is For Uploading image
 const handleImageUpload = async (req, res) => {
   try {
     const b64 = Buffer.from(req.file.buffer).toString("base64");
@@ -19,4 +21,136 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-module.exports = handleImageUpload;
+// This Controller for add a new Product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStocks,
+    } = req.body;
+
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStocks,
+    });
+
+    await newlyCreatedProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedProduct,
+      message: "Product Created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
+// This constroller for fetching all the added Product
+
+const fetchAllProducts = async (req, res) => {
+  try {
+    const listOfProduct = await Product.find({});
+    res.status(201).json({ success: true, data: listOfProduct });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
+// This controller for edit product
+const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStocks,
+    } = req.body;
+
+    const findProduct = await Product.findById(id);
+    if (!findProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    findProduct.title = title || findProduct.title;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.brand = brand || findProduct.brand;
+    findProduct.price = price || findProduct.price;
+    findProduct.salePrice = salePrice || findProduct.salePrice;
+    findProduct.totalStocks = totalStocks || findProduct.totalStocks;
+    findProduct.image = image || findProduct.image;
+
+    await findProduct.save();
+
+    res.status(201).json({
+      success: true,
+      data: findProduct,
+      message: "Product Updated Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
+// This controller is for Delete Product
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({
+      success: ture,
+      message: "Product deleted SuccessFully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  editProduct,
+  fetchAllProducts,
+  deleteProduct,
+};
